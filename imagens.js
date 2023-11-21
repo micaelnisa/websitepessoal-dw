9d517d941a45a9c2f5fa75c76dabe10df4dd9424
micaelnisa <mikassantosnisa@gmail.com>
2023-11-21 15:37:56 +0000

    animação

--- imagens.js
+++ imagens.js
@@ -1,6 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
     let factos = document.querySelectorAll(".facto");
+    let imagensFactos = document.querySelectorAll(".image-facto");
 
+    //console.log(factos)
+
+    //ADICIONA FACTOS
     document.querySelector(".factos").addEventListener("click", function (event) {
      
         let factosArray = Array.from(factos);
@@ -21,45 +25,60 @@ document.addEventListener("DOMContentLoaded", function () {
             randomFacto.style.display = "block";
         }
     });
-});
 
+    //MUDA O ESTILO
+    let index = 0;
+    let videos = document.querySelectorAll('.fundo video');
+    let clickCount = 0;
 
+    let movimentosSexy = ["movendo", "rodar-e-mover", "imagem-facto"];
 
-        let index = 0;
-        let videos = document.querySelectorAll('.fundo video');
-        let clickCount = 0;
+    let botaoMudarVideo = document.getElementById("trocar");
 
-        function playNextVideo() {
+    botaoMudarVideo.addEventListener("click", function(){
+
+        clickCount++;
+
+        imagensFactos.forEach(function(imagensFactos){
+            imagensFactos.classList.add(movimentosSexy[clickCount-1])
+            imagensFactos.classList.remove(movimentosSexy[clickCount-2])
+        })
+        
+
+        if (clickCount > 3) {
+            // Oculta todos os vídeos
+            videos.forEach(function (video) {
+                video.pause();
+                video.currentTime = 0;
+                video.style.display = 'none';
+            });
+
             
-            clickCount++;
+            clickCount = 0;
+        } else {
+            // Pausa e reinicia todos os vídeos
+            videos.forEach(function (video) {
+                video.pause();
+                video.currentTime = 0;
+                video.style.display = 'none';
+            });
 
-            
-            if (clickCount > 3) {
-                // Oculta todos os vídeos
-                videos.forEach(function (video) {
-                    video.pause();
-                    video.currentTime = 0;
-                    video.style.display = 'none';
-                });
+            // Atualiza o índice para o próximo vídeo
+            index  = (index  + 1) % videos.length;
 
-               
-                clickCount = 0;
-            } else {
-                // Pausa e reinicia todos os vídeos
-                videos.forEach(function (video) {
-                    video.pause();
-                    video.currentTime = 0;
-                    video.style.display = 'none';
-                });
+            // Mostra o próximo vídeo
+            videos[index].style.display = 'block';
 
-                // Atualiza o índice para o próximo vídeo
-                index  = (index  + 1) % videos.length;
-
-                // Mostra o próximo vídeo
-                videos[index].style.display = 'block';
-
-                // Ativa o som e reproduz o vídeo
-                videos[index].muted = false;
-                videos[index] .play();
-            }
+            // Ativa o som e reproduz o vídeo
+            videos[index].muted = false;
+            videos[index] .play();
         }
+    })
+
+
+    //MUDA O VIDEO A TOCAR
+    function playNextVideo() {
+
+    }
+
+});
